e1e7f57c968d686be86a6934e974a644116fa88b
mohamed amine <MohamedAmine1C@gmail.com>
2017-09-19 19:26:53 +0100

    add error reporting

--- defaults.php
+++ defaults.php
@@ -3,6 +3,7 @@
 session_start();
 ob_start();
 date_default_timezone_set(getenv('TIMEZONE'));
+error_reporting(getenv('ERROR_REPORTING'));
 
 set_exception_handler(function (Throwable $e) {
     $Msg = $e->getMessage();
