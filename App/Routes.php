0b856ac992bffd38298639618c0630937df6f104
Mohamed Amine <MohamedAmine1C@gmail.com>
2018-02-28 19:48:16 +0100

    remove comments from Routes

--- App/Routes.php
+++ App/Routes.php
@@ -3,16 +3,3 @@
 $this->get('', function () {
     view('index');
 });
-
-// post example
-// $this->post('test', 'controller@test');
-
-// resource example
-// $this->resource('user', 'controller');
-
-// group example
-//$this->group('myprefix/', function () {
-//     $this->get('create', function () {
-//         view('create');
-//     });
-// });
