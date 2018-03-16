774006778f0acfc53b27f884fd9301472f99ab31
Mohamed Amine <MohamedAmine1C@gmail.com>
2018-03-16 16:13:06 +0100

    add commenting

--- App/App.php
+++ App/App.php
@@ -1,9 +1,17 @@
 <?php
 
+/**
+ * Created Services that are critical to our application.
+ *
+ */
+
 container()->bind('Router', function() {
     return new Router\Router(rootDir().'/App/Routes.php');
 });
 
+container()->bind('Request', function() {
+    return new Request\Request();
+});
 container()->bind('Compiler', function() {
     return new Kernel\Compiler\Compiler();
 });
--- App/Services.php
+++ App/Services.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * Define services aliases and service providers
+ * that will be loaded On application start.
+ *
+ */
+
 return [
 
     'ServiceLocators' => [
