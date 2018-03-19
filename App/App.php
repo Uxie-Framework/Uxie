08b2dca6f63cb67dfac5b03a449073fe92b19832
Mohamed Amine <MohamedAmine1C@gmail.com>
2018-03-19 13:44:14 +0100

    change services order

--- App/App.php
+++ App/App.php
@@ -5,17 +5,18 @@
  *
  */
 
-container()->bind('Router', function() {
-    return new Router\Router(rootDir().'/App/Routes.php');
+container()->bind('Dotenv', function() {
+    return new Dotenv\Dotenv(__DIR__.'/../');
+});
+
+container()->bind('Compiler', function() {
+    return new Kernel\Compiler\Compiler();
 });
 
 container()->bind('Request', function() {
     return new Request\Request();
 });
-container()->bind('Compiler', function() {
-    return new Kernel\Compiler\Compiler();
-});
 
-container()->bind('Dotenv', function() {
-    return new Dotenv\Dotenv(__DIR__.'/../');
+container()->bind('Router', function() {
+    return new Router\Router(rootDir().'/App/Routes.php');
 });
