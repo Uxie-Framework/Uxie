770d16e6f0703d3e45e50cb13a526da6a41a82fe
Mohamed Amine <MohamedAmine1C@gmail.com>
2018-09-12 17:29:38 -0400

    add default & automatic fields to statistics migration

--- database/migrations/20180209152652_statistics.php
+++ database/migrations/20180209152652_statistics.php
@@ -30,20 +30,26 @@ class Statistics extends AbstractMigration
     {
         $hits = $this->table('statistics_hits', ['id' => false ,'primary_key' => 'id']);
         $hits
-        ->addColumn('id', 'string', ['limit' => 30])
-        ->addColumn('ip', 'string', ['limit' => 16])
-        ->addColumn('os', 'string', ['limit' => 30])
-        ->addColumn('browser', 'string', ['limit' => 30])
-        ->addColumn('currenturl', 'string', ['limit' => 255])
-        ->addColumn('previousurl', 'string', ['limit' => 255, 'null' => true])
-        ->addColumn('date', 'datetime')
-        ->addColumn('memory', 'integer')
-        ->create();
+         ->addColumn('id', 'string', ['limit' => 30])
+         ->addColumn('ip', 'string', ['limit' => 16])
+         ->addColumn('os', 'string', ['limit' => 30])
+         ->addColumn('browser', 'string', ['limit' => 30])
+         ->addColumn('currenturl', 'string', ['limit' => 255])
+         ->addColumn('previousurl', 'string', ['limit' => 255, 'null' => true])
+         ->addColumn('date', 'datetime')
+         ->addColumn('memory', 'integer')
+         ->addColumn('softdelete', 'boolean')
+         ->addColumn('created_at', 'datetime')
+         ->addColumn('deleted_at', 'datetime')
+         ->create();
 
         $uniq = $this->table('statistics_uniq', ['id' => false ,'primary_key' => 'id']);
         $uniq
-        ->addColumn('id', 'string', ['limit' => 30])
-        ->addColumn('hits', 'integer', ['limit' => 5])
-        ->create();
+         ->addColumn('id', 'string', ['limit' => 30])
+         ->addColumn('hits', 'integer', ['limit' => 5])
+         ->addColumn('softdelete', 'boolean', ['default' => 0])
+         ->addColumn('created_at', 'datetime')
+         ->addColumn('deleted_at', 'datetime')
+         ->create();
     }
 }
