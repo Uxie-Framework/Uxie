8007d66be02f06dce3aded5b8aba78515d70a8cd
Mohamed Amine <MohamedAmine1C@gmail.com>
2018-09-29 21:38:28 -0400

    fix statistics bug in migration

--- database/migrations/20180209152652_statistics.php
+++ database/migrations/20180209152652_statistics.php
@@ -5,51 +5,30 @@ use Phinx\Migration\AbstractMigration;
 
 class Statistics extends AbstractMigration
 {
-    /**
-     * Change Method.
-     *
-     * Write your reversible migrations using this method.
-     *
-     * More information on writing migrations is available here:
-     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
-     *
-     * The following commands can be used in this method and Phinx will
-     * automatically reverse them when rolling back:
-     *
-     *    createTable
-     *    renameTable
-     *    addColumn
-     *    renameColumn
-     *    addIndex
-     *    addForeignKey
-     *
-     * Remember to call "create()" or "update()" and NOT "save()" when working
-     * with the Table class.
-     */
     public function change()
     {
         $hits = $this->table('statistics_hits', ['id' => false ,'primary_key' => 'id']);
         $hits
-         ->addColumn('id', 'string', ['limit' => 30])
-         ->addColumn('ip', 'string', ['limit' => 16])
-         ->addColumn('os', 'string', ['limit' => 30])
-         ->addColumn('browser', 'string', ['limit' => 30])
-         ->addColumn('currenturl', 'string', ['limit' => 255])
-         ->addColumn('previousurl', 'string', ['limit' => 255, 'null' => true])
-         ->addColumn('date', 'datetime')
-         ->addColumn('memory', 'integer')
-         ->addColumn('softdelete', 'boolean')
-         ->addColumn('created_at', 'datetime')
-         ->addColumn('deleted_at', 'datetime')
-         ->create();
+        ->addColumn('id', 'string', ['limit' => 30])
+        ->addColumn('ip', 'string', ['limit' => 16])
+        ->addColumn('os', 'string', ['limit' => 30])
+        ->addColumn('browser', 'string', ['limit' => 30])
+        ->addColumn('currenturl', 'string', ['limit' => 255])
+        ->addColumn('previousurl', 'string', ['limit' => 255, 'null' => true])
+        ->addColumn('date', 'datetime')
+        ->addColumn('memory', 'integer')
+        ->addColumn('softdelete', 'boolean', ['default' => 0])
+        ->addColumn('created_at', 'datetime')
+        ->addColumn('deleted_at', 'datetime', ['null' => true])
+        ->create();
 
         $uniq = $this->table('statistics_uniq', ['id' => false ,'primary_key' => 'id']);
         $uniq
-         ->addColumn('id', 'string', ['limit' => 30])
-         ->addColumn('hits', 'integer', ['limit' => 5])
-         ->addColumn('softdelete', 'boolean', ['default' => 0])
-         ->addColumn('created_at', 'datetime')
-         ->addColumn('deleted_at', 'datetime')
-         ->create();
+        ->addColumn('id', 'string', ['limit' => 30])
+        ->addColumn('hits', 'integer', ['limit' => 5])
+        ->addColumn('softdelete', 'boolean', ['default' => 0])
+        ->addColumn('created_at', 'datetime')
+        ->addColumn('deleted_at', 'datetime', ['null' => true])
+        ->create();
     }
 }
