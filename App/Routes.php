4a171468d9739c077827de54643ca6d35c21bc77
Mohamed Amine <MohamedAmine1C@gmail.com>
2019-01-15 10:14:09 -0500

    add request and response to routes file

--- App/Routes.php
+++ App/Routes.php
@@ -1,6 +1,9 @@
 <?php
 
-$this->get('/', function ($request, $response) {
+use Request\Request as Request;
+use Response\Response as Response;
+
+$this->get('/', function (Request $request, Response $response) {
     $response->view('index')->send();
     $response->end();
 });
