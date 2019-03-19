8cf40861ebad02b8e1caf5403a5960a1b62cc949
Mohamed Amine <MohamedAmine1C@gmail.com>
2019-03-18 21:27:55 -0400

    add request & response to statistics middleware

--- App/Middlewares/Statistics.php
+++ App/Middlewares/Statistics.php
@@ -1,10 +1,12 @@
 <?php
-
 namespace Middleware;
+
+use Request\Request as Request;
+use Response\Response as Response;
 
 class Statistics
 {
-    public function __construct()
+    public function __construct(Request $request, Response $response)
     {
         container()->build('Statistics\Visit');
     }
