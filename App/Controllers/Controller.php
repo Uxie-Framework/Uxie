a858131c005b1d409f669d7973ee018e782be482
Mohamed Amine <cheribet.amine@gmail.com>
2019-12-13 19:34:56 -0500

    add nedded imports to the default controller

--- App/Controllers/Controller.php
+++ App/Controllers/Controller.php
@@ -2,6 +2,10 @@
 
 namespace Controller;
 
+use Controller\Controller as Controller;
+use Request\Request as Request;
+use Response\Response as Response;
+
 abstract class Controller
 {
     public function __construct()
