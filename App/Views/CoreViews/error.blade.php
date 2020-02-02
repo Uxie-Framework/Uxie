bb9865df9b41547b09e9c0584fee22de9f264ac3
Mohamed Amine <cheribet.amine@gmail.com>
2020-02-01 20:32:21 -0500

    improve error view

--- App/Views/CoreViews/error.blade.php
+++ App/Views/CoreViews/error.blade.php
@@ -17,7 +17,14 @@
   <body>
     <h1 style="font-size: 7vw;color: rgb(236, 18, 103)">Error</h1>
     <h2 style="font-size: 5vw;">{{$code}}</h2><br>
-    <h2>Error : {{$error}}</h2><br>
+    <h2>Error : {{$error}} </h2><br>
+    @if (isset($file))
+        <h2>File :  {{$file}}  </h2><br>
+    @endif
+    @if (isset($line))
+        <h2>Line :  {{$line}}  </h2><br>
+    @endif
+
     <h4>Please contact admin : <br><br>{{ getenv('ADMIN_EMAIL') }}</h4>
   </body>
 </html>
