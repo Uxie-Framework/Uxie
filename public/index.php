f774483f4c03cfbff01513a49277e47848288056
Amin Cheribet <amin.cheribet2@gmail.com>
2023-05-28 11:41:33 -0400

    Update index.php

--- public/index.php
+++ public/index.php
@@ -6,10 +6,10 @@
  * @author M.Amine Cheribet <cheribet.amine@gmail.com>
  */
 
-// import composer autoloader
+// Import composer autoloader.
 require_once __DIR__.'/../vendor/autoload.php';
 
-// create IOC container
+// Create IOC container.
 IOC\IOC::createContainer();
 
 container()->bind('Dotenv', function () {
@@ -17,16 +17,16 @@ container()->bind('Dotenv', function () {
 });
 container()->Dotenv->load();
 
-// Load configuration
+// Load configuration.
 require rootDir().'defaults.php';
 
-// Preparing for starting application
+// Preparing for starting application.
 container()->build('Kernel\Kernel');
 
 container()->Kernel->prepare();
 
-// Start the application
+// Start the application.
 container()->Kernel->start();
 
-// Stop the application
+// Stop the application.
 container()->Kernel->stop();
